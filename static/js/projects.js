be3c83f6108ae88b38f6e0e662acbe4848c51925
Ivan Wang <icw@cmu.edu>
2014-07-20 21:04:58 -0700

    Add spire and collaborators

--- static/js/projects.js
+++ static/js/projects.js
@@ -14,6 +14,7 @@ angular.module('navignaw.projects', [])
             url: "http://get-matched.herokuapp.com/",
             description: "A discovery platform for small business owners. This <a href='https://locu.com/' target='_blank'>Locu</a>/<a href='http://www.gotinder.com/' target='_blank'>Tinder</a> mashup allows businesses to find each other and network with mutual interest matching.",
             note: "Built with Ruby on Rails. First place winner at 2014 GoDaddy Intern Hackathon!",
+            collaborators: ["Sherman Leung", "Kevin Casey", "Sophia Chung", "Don Mai", "Pushkar Prateek"],
             banner: "static/assets/getmatched/banner.png",
             screenshots: [
                 {url: "static/assets/getmatched/home.png", caption: "Landing Page"},
@@ -31,6 +32,7 @@ angular.module('navignaw.projects', [])
             url: "https://www.cmucsharp.com",
             description: "Interactive website for college a cappella group.",
             note: "Technologies: Ruby on Rails, CoffeeScript",
+            collaborators: ["Cary Yang", "Emily Yeh"],
             banner: "static/assets/cmucsharp/banner.png",
             screenshots: [
                 {url: "static/assets/cmucsharp/logo.png", caption: "Logo"}
@@ -46,6 +48,7 @@ angular.module('navignaw.projects', [])
             url: "http://prezi.com/pcmsvy2rj4e2/project-surfboard/",
             description: "Digital wallboard webapp for tracking JIRA stories and bugs. Features NoSQL database, Java server to send and retrieve data via RESTful API, as well as a customizable front-end service with AngularJS to display issues.",
 			note: "Additional feature: coded to accept Leap Motion gestures as well as keyboard/mouse input!",
+            collaborators: [],
             banner: "static/assets/surfboard/banner.png",
             screenshots: [
                 {url: "static/assets/surfboard/surfboard.png", caption: "Surfboard"}
@@ -61,6 +64,7 @@ angular.module('navignaw.projects', [])
             url: "https://hacknbash.herokuapp.com",
             description: "Explore a remote directory as a robot in this cute gamified FTP client webapp. Features the ability to <kbd>rm</kbd> and <kbd>cat</kbd> (remove and print) files, as well as <kbd>cd</kbd> (change directories).",
             note: "Developed with Flask and CreateJS for a 24-hour hackathon, TartanHacks; winner of Capital One sponsor prize.",
+            collaborators: ["Alex Mermelstein", "Esther Wang", "Jun Huo"],
             banner: "static/assets/hacknbash/banner.png",
             screenshots: [
                 {url: "static/assets/hacknbash/game.png", caption: "Game Map"},
@@ -77,6 +81,7 @@ angular.module('navignaw.projects', [])
             url: null,
             description: "Online software and game development portfolio, i.e. this site! Open-sourced on <a href='https://github.com/navignaw/navignaw' target='_blank'>github</a>.",
             note: "Built with Flask and AngularJS.",
+            collaborators: [],
             banner: "static/assets/navignaw/banner.png",
             screenshots: [],
             embed: null,
@@ -85,6 +90,27 @@ angular.module('navignaw.projects', [])
 
         /* Mobile Apps */
         {
+            name: "Spire",
+            id: "spire",
+            date: "July 2014",
+            datetime: new Date(2014, 7, 20),
+            url: "",
+            description: "Connect the world through geocaching a virtual pet! Spire features the creation of a virtual pet that you can take pictures with and drop off in a location. As other users pick up your pet, track its progress around the world!",
+            note: "Created with Parse and Objective-C for native iOS at Greylock Hackfest 2014.",
+            collaborators: ["Lucy Guo", "Nive Jayasekar", "Jeffrey Zhang"],
+            banner: "",
+            screenshots: [
+                /*{url: "static/assets/nommit/app.png", caption: "Main Screen"},
+                {url: "static/assets/nommit/restaurants.png", caption: "Restaurants Near You"},
+                {url: "static/assets/nommit/menu.png", caption: "Menu"},
+                {url: "static/assets/nommit/order.png", caption: "Order Confirmation"},
+                {url: "static/assets/nommit/venmo.png", caption: "Venmo Payment"},
+                {url: "static/assets/nommit/text.png", caption: "Text Notification"}*/
+            ],
+            embed: null,
+            type: "mobile"
+        },
+        {
             name: "nommit",
             id: "nommit",
             date: "Oct 2013",
@@ -92,6 +118,7 @@ angular.module('navignaw.projects', [])
             url: "http://hackmit.challengepost.com/submissions/17868-nommit",
             description: "Native iOS app for crowdsourced social food delivery. Features API from Firebase for storing and displaying real-time order data, Google Maps for tracking customers and couriers, Locu for querying a list of best nearby restaurants and menus, Venmo for order payments and tips, and Twilio for text notifications.",
             note: "Built during a 24-hour hackathon, HackMIT; winner of Venmo's Makes Life So Easy award.",
+            collaborators: ["Lucy Guo", "Gregory Rose", "Angela Zhang"],
             banner: "static/assets/nommit/banner.png",
             screenshots: [
                 {url: "static/assets/nommit/app.png", caption: "Main Screen"},
@@ -112,6 +139,7 @@ angular.module('navignaw.projects', [])
             url: "http://mhackswinter2014.challengepost.com/submissions/20243-spontaneity",
             description: "The spontaneous hangout app, <em>Spontaneity</em> suggests nine categories of activities to participate in, including nearby parties, exercise spots, and even baking recipes from Pinterest. Through the creation of Facebook events to text-message notifications to nearby users, Spontaneity aims to liven up an otherwise boring day.",
 			note: "Developed in 36 hours during MHacks; winner of Yahoo's Best Mobile App.",
+            collaborators: ["Lucy Guo", "Gregory Rose", "Angela Zhang"],
             banner: "static/assets/spontaneity/banner.png",
             screenshots: [
                 {url: "static/assets/spontaneity/app.png", caption: "Main Screen"},
@@ -131,6 +159,7 @@ angular.module('navignaw.projects', [])
             url: "http://pennapps2014s.challengepost.com/submissions/20856-edugames-create-educational-games-with-0-programming-knowledge",
             description: "<em>EduGames</em> allows teachers to create engaging interactive games tailored to their curriculum with no programming knowledge. Features a <a href='https://edugames.firebaseapp.com' target='_blank'>webapp</a> where teachers can choose from existing game templates and fill in questions, as well as a native iPad app where students can log in and play customized games created by their teachers.",
             note: "Coded with love during PennApps (Valentines' Edition); winner of IndieGogo prize.",
+            collaborators: ["Lucy Guo", "Gregory Rose", "Angela Zhang"],
             banner: "static/assets/edugames/banner.png",
             screenshots: [
                 {url: "static/assets/edugames/app.png", caption: "Main Screen"},
@@ -153,6 +182,7 @@ angular.module('navignaw.projects', [])
             url: "http://www.hbgames.org/forums/viewtopic.php?f=77&t=39133",
             description: "My first project features a time-traveling puzzle RPG built in RPG Maker XP. Enjoy 2-4 hours of puzzles and turn-based battles against a variety of foes.",
             note: "",
+            collaborators: [],
             banner: "static/assets/omnisia/banner.png",
             screenshots: [
                 {url: "static/assets/omnisia/lutrena.png", caption: "Lutrena"},
@@ -172,6 +202,7 @@ angular.module('navignaw.projects', [])
             url: "http://www.hbgames.org/forums/viewtopic.php?p=852052",
             description: "<em>Scavenger</em> is a 2D puzzle-action-adventure game, a la the Legend of Zelda. Explore a vast pixel-perfect world equipped with tools and the unique ability to visualize the past.",
             note: "Created in RPG Maker XP and coded in Ruby. Scavenger is currently in development!",
+            collaborators: [],
             banner: "static/assets/scavenger/banner.png",
             screenshots: [
                 {url: "static/assets/scavenger/library.png", caption: "Burning Library"},
@@ -193,6 +224,7 @@ angular.module('navignaw.projects', [])
             url: "http://www.hbgames.org/forums/viewtopic.php?f=299&t=76684",
             description: "Explore a morbid, alien world as a small, innocent <small>NEWT</small>. In the empty labs of the <small>FACILITY</small>, escaping is not as simple as finding the key to the lock. With lasers, keypads, and fingerprint scanners, to say the least, there will be countless challenges facing your freedom-seeking amphibian. Can you creatively solve your way through five difficult trials, realize your role in the sinister plot of a few evil-genius human scientists, and escape?",
 			note: "Submission for the HBGames.org 5-Year Anniversary Contest; built in Adobe Flash and ActionScript.",
+            collaborators: [],
             banner: "static/assets/newt/banner.png",
             screenshots: [],
             embed: {
@@ -211,6 +243,7 @@ angular.module('navignaw.projects', [])
             url: "http://www.gamecreation.org/game/retrograde",
             description: "A reverse platformer that explores the return of a hero after his rise to power.",
             note: "Created with Flixel, for Adobe Flash.",
+            collaborators: ["CMU Game Creation Society"],
             banner: "static/assets/retrograde/banner.png",
             screenshots: [
                 {url: "static/assets/retrograde/crumbles.png", caption: "Crumbling Platforms"},
@@ -235,6 +268,7 @@ angular.module('navignaw.projects', [])
             url: "http://globalgamejam.org/2014/games/illuminate",
             description: "Explore a changing world in this experimental 3D puzzle-adventure, one which revolves around the perception of one's surroundings.",
 			note: "Developed in Unity and C# for the 2014 Global Game Jam.",
+            collaborators: ["CMU Game Creation Society"],
             banner: "static/assets/illuminate/banner.png",
             screenshots: [
                 {url: "static/assets/illuminate/flashlight.jpg", caption: "The Flashlight"},
